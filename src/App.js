802f608c3a992af1eaaa1ec66a6ba110783016d6
Utloan2914 <loan.huynh25@student.passerellesnumeriques.org>
2024-05-19 17:46:59 +0700

    Start fodel layout

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { useEffect } from "react";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+
+function App() {
+  // Use the 'useEffect' hook to perform side effects in function components
+  useEffect(() => {
+    // Define an asynchronous function 'fetchData' to fetch data from the GitHub API
+    const fetchData = async () => {
+      try {
+        // Use the 'axios' library to make a GET request to the GitHub API endpoint
+        const response = await axios.get("https://api.github.com/users");
+        // Log the fetched data to the console
+        console.log("GitHub Users:", response.data);
+      } catch (error) {
+        // Log an error message if there's an issue fetching data
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    // Call the 'fetchData' function when the component mounts
+    fetchData();
+  }, []); // The empty dependency array ensures that 'useEffect' runs only once when the component mounts
+
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="container">
+        <h1>Hello from React</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
