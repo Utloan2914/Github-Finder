20733fed8ff5af7039c8668e816342b747f95a2b
Utloan2914 <loan.huynh25@student.passerellesnumeriques.org>
2024-05-19 18:03:20 +0700

    Continute fodel users

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
 
 function App() {
+  // Use the 'useState' hook to create a state variable 'users' and a function 'setUsers' to update it
+  const [users, setUsers] = useState([]);
+
   // Use the 'useEffect' hook to perform side effects in function components
   useEffect(() => {
     // Define an asynchronous function 'fetchData' to fetch data from the GitHub API
@@ -11,6 +15,8 @@ function App() {
       try {
         // Use the 'axios' library to make a GET request to the GitHub API endpoint
         const response = await axios.get("https://api.github.com/users");
+        // Update the 'users' state with the fetched data
+        setUsers(response.data);
         // Log the fetched data to the console
         console.log("GitHub Users:", response.data);
       } catch (error) {
@@ -27,7 +33,9 @@ function App() {
     <div className="App">
       <Navbar />
       <div className="container">
-        <h1>Hello from React</h1>
+        <h1>GitHub Users Data</h1>
+        {/* Render the Users component and pass the 'users' state as a prop */}
+        <Users users={users} />
       </div>
     </div>
   );
